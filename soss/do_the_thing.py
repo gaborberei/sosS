f517f65edf7e7ec6449060fd1cc257857e15e620
gaborberei <berei.gabor1@gmail.com>
2022-03-25 19:43:38 +0100

    do doscstring and make installable

--- soss/do_the_thing.py
+++ soss/do_the_thing.py
@@ -1,3 +1,21 @@
 def soss(a_list):
+    """
+    Do the thing
+
+    Squares elements of a lists, add them and square the result
+
+    Params:
+    -------
+
+    a_list : list
+        A list of numbers
+
+    Return:
+    -------
+
+    result : float
+        A number 
+
+    """
 
     return sum([i ** 2 for i in a_list]) ** 2
